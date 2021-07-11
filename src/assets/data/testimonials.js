e093c1d471c1073c0236232d80543ac6611c4ff2
Andrewmoo <andrewmoo98@gmail.com>
2021-07-11 11:51:58 +0100

    Changed testimonials

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -3,7 +3,7 @@ const testimonials = [
     id: 1,
     name: 'Andrew Bell',
     title: 'CEO',
-    org: 'BigHogs Inc.',
+    org: 'Blah Inc.',
     desc:
       'Lee is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
@@ -11,15 +11,15 @@ const testimonials = [
     id: 2,
     name: 'Lee Kambo',
     title: 'Co-founder',
-    org: 'Robbies Balls',
+    org: 'Rogue Gallery',
     desc:
       'Really amazing communication skills. Always understands what I am trying to achieve. Also, his work is really amazing. Does really high-quality work.',
   },
   {
     id: 3,
-    name: 'Sussy Bakka',
-    title: 'CSO',
-    org: 'Impostor',
+    name: 'Jack Mooney',
+    title: 'CFO',
+    org: 'ShopPlace Ltd.',
     desc:
       'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
   },
