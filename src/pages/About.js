4b80b24f213d2643f33a0f6f09271e43646f2516
Andrewmoo <andrewmoo98@gmail.com>
2021-07-23 17:17:37 +0100

    Changed About image again

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PText from '../components/PText';
 import Button from '../components/Button';
-import AboutImg from '../assets/images/LeeAbout.jpg';
+import AboutImg from '../assets/images/LeeAbout2.jpg';
 import AboutInfoItems from '../components/AboutInfoItem';
 import ContactBanner from '../components/ContactBanner';
 
